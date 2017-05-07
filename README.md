d5fe627dc32f9fbaaa8b9302b1c279de087c0d0c
Akhi Syabab <akhisyabab@gmail.com>
2017-05-07 20:35:27 +0700

    flask-api-mysql-simple first commi

---
+++ README.md
@@ -0,0 +1,12 @@
+# Simple Flask API 
+Simple flask api with mysql
+
+1. clone repo: git clone https://github.com/akhisyabab/flask-api-mysql-simple.git
+2. create virtual environment : virtualenv env
+3. source env/bin/activate
+4. pip install -r requirements.txt
+5. Setting your database config in api.py
+6. Open your mysql console and follow step in db_procedure.txt
+7. python api.py
+ 
+
---
+++ api.py
@@ -0,0 +1,189 @@
+from flask import Flask
+from flask_restful import Resource, Api
+from flask_restful import reqparse
+from flaskext.mysql import MySQL
+
+
+
+mysql = MySQL()
+app = Flask(__name__)
+
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_DB'] = 'flask-api-mysql-simple'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+
+
+mysql.init_app(app)
+
+api = Api(app)
+
+
+class AddItem(Resource):
+    def post(self):
+        try: 
+            # Parse the arguments
+            parser = reqparse.RequestParser()
+            parser.add_argument('id', type=str)
+            parser.add_argument('item', type=str)
+            args = parser.parse_args()
+
+            _userId = args['id']
+            _item = args['item']
+
+            print _userId;
+
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            cursor.callproc('sp_AddItems',(_userId,_item))
+            data = cursor.fetchall()
+
+            conn.commit()
+            return {'StatusCode':'200','Message': 'Success'}
+
+        except Exception as e:
+            return {'error': str(e)}
+                  
+
+class GetAllItems(Resource):
+    def get(self):
+        try: 
+            # Parse the arguments
+            parser = reqparse.RequestParser()
+            parser.add_argument('id', type=str)
+            args = parser.parse_args()
+
+            _userId = args['id']
+
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            cursor.callproc('sp_GetAllItems',(_userId,))
+            data = cursor.fetchall()
+
+            items_list=[];
+            for item in data:
+                i = {
+                    'Id':item[0],
+                    'Item':item[1]
+                }
+                items_list.append(i)
+
+            return {'StatusCode':'200','Items':items_list}
+
+        except Exception as e:
+            return {'error': str(e)}
+
+
+class DeleteItem(Resource):
+    def delete(self):
+        try: 
+            # Parse the arguments
+            parser = reqparse.RequestParser()
+            parser.add_argument('id', type=str)
+            args = parser.parse_args()
+
+            _Id = args['id']
+
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            cursor.callproc('sp_DeleteItem',(_Id,))
+            data = cursor.fetchall()
+
+            conn.commit()
+
+            return {'StatusCode':'deleted'}
+
+        except Exception as e:
+            return {'error': str(e)}
+
+class UpdateItem(Resource):
+    def put(self):
+        try: 
+            # Parse the arguments
+            parser = reqparse.RequestParser()
+            parser.add_argument('id', type=str)
+            parser.add_argument('item', type=str, help='Item Name')
+            args = parser.parse_args()
+
+            _Id = args['id']
+            _item = args['item']
+
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            cursor.callproc('sp_UpdateItem',(_Id,_item))
+            data = cursor.fetchall()
+
+            conn.commit()
+
+            return {'StatusCode':'200'}
+
+        except Exception as e:
+            return {'error': str(e)}
+
+
+
+class CreateUser(Resource):
+    def post(self):
+        try:
+            # Parse the arguments
+            parser = reqparse.RequestParser()
+            parser.add_argument('email', type=str, help='Email address to create user')
+            parser.add_argument('password', type=str, help='Password to create user')
+            args = parser.parse_args()
+
+            _userEmail = args['email']
+            _userPassword = args['password']
+
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            cursor.callproc('spCreateUser',(_userEmail,_userPassword))
+            data = cursor.fetchall()
+
+            if len(data) is 0:
+                conn.commit()
+                return {'StatusCode':'200','Message': 'User creation success'}
+            else:
+                return {'StatusCode':'1000','Message': str(data[0])}
+
+        except Exception as e:
+            return {'error': str(e)}
+
+class AuthenticateUser(Resource):
+    def post(self):
+        try:
+            # Parse the arguments
+
+            parser = reqparse.RequestParser()
+            parser.add_argument('email', type=str, help='Email address for Authentication')
+            parser.add_argument('password', type=str, help='Password for Authentication')
+            args = parser.parse_args()
+
+            _userEmail = args['email']
+            _userPassword = args['password']
+
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            cursor.callproc('sp_AuthenticateUser',(_userEmail,))
+            data = cursor.fetchall()
+
+            
+            if(len(data)>0):
+                if(str(data[0][2])==_userPassword):
+                    return {'status':200,'UserId':str(data[0][0])}
+                else:
+                    return {'status':100,'message':'Authentication failure'}
+
+        except Exception as e:
+            return {'error': str(e)}
+
+
+api.add_resource(CreateUser, '/CreateUser')
+api.add_resource(AuthenticateUser, '/AuthenticateUser')
+api.add_resource(AddItem, '/AddItem')
+api.add_resource(GetAllItems, '/GetAllItems')
+api.add_resource(DeleteItem, '/DeleteItem')
+api.add_resource(UpdateItem, '/UpdateItem')
+
+if __name__ == '__main__':
+    app.run(debug=True)
